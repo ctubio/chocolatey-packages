c5a1bd39de498884f9a2ef4b016bac5f6cc1675d
Carles Tubio <carles.tubio@lecreuset.com>
2015-03-29 08:08:49 +0200

    Added visualregexp package.

---
+++ visualregexp.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'visualregexp.portable'
+$url = 'http://laurent.riesterer.free.fr/regexp/visual_regexp-3.1.exe'
+try {
+  $fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\visual_regexp-3.1.exe"
+
+  Get-ChocolateyWebFile $packageName "$fileFullPath" $url
+  Install-ChocolateyDesktopLink "$fileFullPath"
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
