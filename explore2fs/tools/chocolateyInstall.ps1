7808c297dbfe2249582df26262764a0576e07be5
Carles Tubio <carles.tubio@lecreuset.com>
2015-03-29 10:07:59 +0200

    Added explore2fs package.

---
+++ explore2fs/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'explore2fs'
+$url = 'http://www.chrysocome.net/downloads/explore2fs-1.07.zip'
+try {
+  $installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Install-ChocolateyZipPackage $packageName $url "$installPath"
+  
+  $exeFile = Join-Path "$installPath" "explore2fs.exe"
+  Install-ChocolateyDesktopLink $exeFile
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}me"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+} throw
+}
