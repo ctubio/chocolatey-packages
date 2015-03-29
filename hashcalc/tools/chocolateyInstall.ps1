cd36b4f74d152a70419df108d63ff860dfaa2868
Carles Tubio <carles.tubio@lecreuset.com>
2015-03-29 08:09:03 +0200

    Added hashcalc package.

---
+++ hashcalc/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'hashcalc'
+$url = 'http://www.slavasoft.com/zip/hashcalc.zip'
+try {
+  $installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Install-ChocolateyZipPackage 'hashcalc' $url "$installPath"
+  
+  $installFilePath = get-childitem "$installPath" -recurse -include *.exe | select -First 1
+  Install-ChocolateyInstallPackage 'setup' 'exe' '/SILENT' "$installFilePath"
+ 
+  Remove-Item "$installFilePath"
+ 
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+} throw
+}
---
+++ hashcalc/tools/chocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+﻿$packageName = 'hashcalc'
+$file = (Get-ItemProperty HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\HashCalc_is1 UninstallString).UninstallString;
+Uninstall-ChocolateyPackage "$packageName" 'EXE' -SilentArgs '/SILENT' -file $fileile $file
